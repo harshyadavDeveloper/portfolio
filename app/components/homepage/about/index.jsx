72c2655a8786908bc42a2713032bb4d79c5c6526
HarshS20-742 <harshyadav5939@gmail.com>
2024-03-26 22:16:13 +0530

    image changes

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -23,12 +23,13 @@ function AboutSection() {
           </p>
         </div>
         <div className="flex justify-center order-1 lg:order-2">
-          <Image
+        <Image
             src={'/picture.png'}
             width={280}
             height={280}
             alt="Harsh Yadav"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
+            style={{ transform: 'rotate(90deg)' }} // Inline CSS for rotation
           />
         </div>
       </div>
